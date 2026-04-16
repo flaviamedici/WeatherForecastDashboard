29975854b7f1b9ddd3fd0d3702803e8c427cd839
flaviamedici <flaviaomedici@gmail.com>
2026-04-16 09:21:27 -0700

    Improvements for the app, added the condition for Sky and images for the different sky conditions

--- backend.py
+++ backend.py
@@ -5,19 +5,16 @@ import os
 load_dotenv()
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={WEATHER_API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict['main']["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
+
 if __name__ == "__main__":
-    print(get_data(place="Seattle", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Seattle", forecast_days=3))
